da8c927a735ab943225388be3226ba71b478347c
mr strange <mr1strange@gmail.com>
2018-07-09 01:07:58 +0200

    premier, retéléchargement phpStorm

--- Views/Responses/message_status.php
+++ Views/Responses/message_status.php
@@ -4,13 +4,13 @@ if($_POST && isset($_POST['message']) && !empty($_POST['message'])){
     $messages = [];
     require_once '../../Models/Model.php';
     $database = new Model();
-    $data['status_message'] = trim($_POST['message']);
-    $data['status_user_id'] = $_SESSION['User']['user_id'];
+    $data['status_message'] = trim(htmlspecialchars($_POST['message']));
+    $data['status_user_id'] = intval($_SESSION['User']['user_id']);
     $req = $database->getTable('status')->save($data);
     if($req){
-        $messages['retour'] =  '<div class="alert alert-success">Mes</div>';
+        $messages['retour'] =  '<div class="alert alert-success">Votre message a bien été posté</div>';
     }else{
-        $messages['retour'] =  '<div class="alert alert-danger">Pro</div>';
+        $messages['retour'] =  '<div class="alert alert-danger">Votre message n\'a pas été posté</div>';
     }
 
 }
